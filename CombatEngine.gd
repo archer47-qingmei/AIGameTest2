1f06ba3dea45159c48ff4b2ed6212c4698c5f0be
Developer <dev@example.com>
2026-04-24 18:22:03 +0800

    refactor: CombatEngine.setup() loads data from .tres files

--- CombatEngine.gd
+++ CombatEngine.gd
@@ -12,45 +12,36 @@ var turn_number: int = 0
 
 var _draw_pile: Array[CardData] = []
 var _discard_pile: Array[CardData] = []
+var _enemy_data: EnemyData
 
 func setup() -> void:
+	_enemy_data = preload("res://data/enemies/jaw_worm.tres")
+	enemy = Combatant.new()
+	enemy.display_name = _enemy_data.display_name
+	enemy.hp = _enemy_data.hp
+	enemy.max_hp = _enemy_data.hp
+	enemy.block = 0
+
 	player = Combatant.new()
 	player.display_name = "玩家"
 	player.hp = 70
 	player.max_hp = 70
 	player.block = 0
 
-	enemy = Combatant.new()
-	enemy.display_name = "颚虫"
-	enemy.hp = 44
-	enemy.max_hp = 44
-	enemy.block = 0
-
+	var strike: CardData = preload("res://data/cards/strike.tres")
+	var defend: CardData = preload("res://data/cards/defend.tres")
+	var bash: CardData = preload("res://data/cards/bash.tres")
 	for i in 4:
-		var c: CardData = CardData.new()
-		c.card_name = "打击"
-		c.cost = 1
-		c.damage = 6
-		c.block = 0
-		_draw_pile.append(c)
-
+		_draw_pile.append(strike.duplicate())
 	for i in 4:
-		var c: CardData = CardData.new()
-		c.card_name = "防御"
-		c.cost = 1
-		c.damage = 0
-		c.block = 5
-		_draw_pile.append(c)
-
-	var bash: CardData = CardData.new()
-	bash.card_name = "重击"
-	bash.cost = 2
-	bash.damage = 8
-	bash.block = 0
-	_draw_pile.append(bash)
+		_draw_pile.append(defend.duplicate())
+	_draw_pile.append(bash.duplicate())
 
 	_draw_pile.shuffle()
 	_start_player_turn()
+
+func get_current_enemy_action() -> EnemyActionData:
+	return _enemy_data.actions[(turn_number - 1) % _enemy_data.actions.size()]
 
 func play_card(card: CardData) -> void:
 	if energy < card.cost:
@@ -97,10 +88,11 @@ func _draw_hand() -> void:
 
 func _do_enemy_turn() -> void:
 	enemy.block = 0
-	if turn_number % 2 == 1:
-		player.take_damage(11)
+	var action: EnemyActionData = _enemy_data.actions[(turn_number - 1) % _enemy_data.actions.size()]
+	if action.type == "attack":
+		player.take_damage(action.value)
 	else:
-		enemy.add_block(6)
+		enemy.add_block(action.value)
 
 func _check_end() -> bool:
 	if enemy.hp <= 0:
