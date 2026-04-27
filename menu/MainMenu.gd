cab6e2688c5b258a02134d635d951a6f1138a0ce
Developer <dev@example.com>
2026-04-27 10:59:36 +0800

    feat: add MainMenu scene and set as main scene

---
+++ menu/MainMenu.gd
@@ -0,0 +1,9 @@
+extends Control
+
+@onready var _btn_start: Button = $BtnStart
+
+func _ready() -> void:
+	_btn_start.pressed.connect(_on_start_pressed)
+
+func _on_start_pressed() -> void:
+	GameManager.go_to_combat()
