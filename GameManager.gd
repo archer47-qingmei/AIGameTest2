c17325849820f0fe845e293fced9a20bf527326a
Developer <dev@example.com>
2026-04-27 11:41:30 +0800

    refactor: move deck init to GameManager, CombatEngine.setup() takes external deck

--- GameManager.gd
+++ GameManager.gd
@@ -1,13 +1,30 @@
 extends Node
 
-enum Phase { MENU, COMBAT }
+enum Phase { MENU, COMBAT, REWARD }
 
 var current_phase: Phase = Phase.MENU
+var player_state: PlayerState
 
 func go_to_combat() -> void:
+	player_state = PlayerState.new()
+	var strike: CardData = preload("res://data/cards/strike.tres")
+	var defend: CardData = preload("res://data/cards/defend.tres")
+	var bash: CardData = preload("res://data/cards/bash.tres")
+	var slash: CardData = preload("res://data/cards/slash.tres")
+	for i in 4:
+		player_state.deck.append(strike.duplicate())
+	for i in 4:
+		player_state.deck.append(defend.duplicate())
+	player_state.deck.append(bash.duplicate())
+	player_state.deck.append(slash.duplicate())
 	current_phase = Phase.COMBAT
 	get_tree().change_scene_to_file("res://combat/CombatScreen.tscn")
 
+func go_to_reward() -> void:
+	current_phase = Phase.REWARD
+	get_tree().change_scene_to_file("res://reward/RewardScreen.tscn")
+
 func go_to_menu() -> void:
+	player_state = null
 	current_phase = Phase.MENU
 	get_tree().change_scene_to_file("res://menu/MainMenu.tscn")
