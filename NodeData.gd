5dfc3da59165085b592483d15eda424a50181e11
Developer <dev@example.com>
2026-04-29 20:24:03 +0800

    refactor: rename col to column in NodeData, add connections direction comment

--- NodeData.gd
+++ NodeData.gd
@@ -5,6 +5,7 @@ enum Type { COMBAT, REST }
 
 var type: Type = Type.COMBAT
 var enemy_data: EnemyData
+# 有向边：指向下一列可到达的节点，仅运行时赋值，不序列化
 var connections: Array[NodeData] = []
-var col: int = 0
+var column: int = 0
 var row: int = 0
