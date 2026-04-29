02716423ce800f122a6dc553d033a79781da1478
Developer <dev@example.com>
2026-04-29 09:34:35 +0800

    refactor: add NodeData resource, replace enemy_sequence with node_sequence in PlayerState

---
+++ NodeData.gd
@@ -0,0 +1,7 @@
+class_name NodeData
+extends Resource
+
+enum Type { COMBAT, REST }
+
+var type: Type = Type.COMBAT
+var enemy_data: EnemyData
--- PlayerState.gd
+++ PlayerState.gd
@@ -5,4 +5,4 @@ var deck: Array[CardData] = []
 var current_node: int = 0
 var hp: int = 70
 var max_hp: int = 70
-var enemy_sequence: Array[String] = []
+var node_sequence: Array[NodeData] = []
