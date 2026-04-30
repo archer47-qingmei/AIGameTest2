21b738d3da5d3df07815cb908c99330179a6014b
Developer <dev@example.com>
2026-04-30 08:49:46 +0800

    refactor: read last_rest_heal from player_state

--- rest/RestScreen.gd
+++ rest/RestScreen.gd
@@ -9,7 +9,7 @@ var _upgrade_buttons: Array[Button] = []
 
 func _ready() -> void:
 	var state: PlayerState = GameManager.player_state
-	_lbl_heal.text = "恢复了 %d 点生命" % GameManager.last_rest_heal
+	_lbl_heal.text = "恢复了 %d 点生命" % GameManager.player_state.last_rest_heal
 	_lbl_hp.text = "当前生命：%d / %d" % [state.hp, state.max_hp]
 	_btn_continue.pressed.connect(GameManager.go_to_map)
 	_populate_upgrade_list()
