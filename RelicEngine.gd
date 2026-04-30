a37af151540162a409a6f7ebe1fdfd6466ac51ed
Developer <dev@example.com>
2026-04-30 12:44:27 +0800

    feat: add RelicEngine static utility

---
+++ RelicEngine.gd
@@ -0,0 +1,19 @@
+class_name RelicEngine
+extends RefCounted
+
+static func apply_combat_start(relics: Array[RelicData], engine: CombatEngine) -> void:
+	for relic: RelicData in relics:
+		if relic.trigger == RelicData.Trigger.COMBAT_START:
+			_apply(relic, engine)
+
+static func apply_turn_start(relics: Array[RelicData], engine: CombatEngine) -> void:
+	for relic: RelicData in relics:
+		if relic.trigger == RelicData.Trigger.TURN_START:
+			_apply(relic, engine)
+
+static func _apply(relic: RelicData, engine: CombatEngine) -> void:
+	match relic.effect_type:
+		"energy":
+			engine.energy += relic.value
+		"heal_hp":
+			engine.player.hp = mini(engine.player.hp + relic.value, engine.player.max_hp)
