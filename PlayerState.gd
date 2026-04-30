2b7f86b193711429cc54c3e25ba84d85f73e5448
Developer <dev@example.com>
2026-04-30 08:45:01 +0800

    refactor: move rest heal logic into PlayerState.apply_rest_heal()

--- PlayerState.gd
+++ PlayerState.gd
@@ -8,3 +8,9 @@ var map_all_nodes: Array[NodeData] = []
 var available_nodes: Array[NodeData] = []
 var completed_nodes: Array[NodeData] = []
 var current_node: NodeData = null
+var last_rest_heal: int = 0
+
+func apply_rest_heal() -> int:
+	last_rest_heal = int(max_hp * 0.3)
+	hp = mini(hp + last_rest_heal, max_hp)
+	return last_rest_heal
