7f087e8fa6aa865af75190d43c7680c093536139
Developer <dev@example.com>
2026-05-01 10:35:50 +0800

    feat: generalize MapGenerator to 10 columns with phase-based node types

--- MapGenerator.gd
+++ MapGenerator.gd
@@ -1,6 +1,8 @@
 class_name MapGenerator
 extends RefCounted
 
+const TOTAL_COLUMNS: int = 10
+
 const COMBAT_ENEMY_PATHS: Array[String] = [
 	"res://data/enemies/jaw_worm.tres",
 	"res://data/enemies/fire_lizard.tres",
@@ -11,67 +13,35 @@ const ELITE_REWARD_RELIC_PATH: String = "res://data/relics/burning_gem.tres"
 const BOSS_REWARD_RELIC_PATH: String = "res://data/relics/life_ring.tres"
 
 static func generate() -> Array[NodeData]:
-	var col0: Array[NodeData] = _make_col0()
-	var col1: Array[NodeData] = _make_col1()
-	var col2: Array[NodeData] = _make_col2()
-	var col3: Array[NodeData] = _make_col3()
-	for nd in col0:
-		nd.connections.assign(col1)
-	for nd in col1:
-		nd.connections.assign(col2)
-	for nd in col2:
-		nd.connections.assign(col3)
+	var columns: Array = []
+	for col in TOTAL_COLUMNS:
+		columns.append(_make_column(col))
+	for i in TOTAL_COLUMNS - 1:
+		for nd in columns[i]:
+			nd.connections.assign(columns[i + 1])
 	var all: Array[NodeData] = []
-	all.append_array(col0)
-	all.append_array(col1)
-	all.append_array(col2)
-	all.append_array(col3)
-	_add_shop_node(all)
+	for col_nodes in columns:
+		all.append_array(col_nodes)
+	_add_shop_nodes(all)
 	return all
 
-static func _make_col0() -> Array[NodeData]:
-	var enemy_paths: Array[String] = COMBAT_ENEMY_PATHS.duplicate()
-	enemy_paths.shuffle()
-	var result: Array[NodeData] = []
-	for row in 2:
+static func _make_column(col: int) -> Array[NodeData]:
+	if col == TOTAL_COLUMNS - 1:
 		var nd := NodeData.new()
 		nd.config = NodeConfig.new()
 		nd.config.type = NodeConfig.Type.COMBAT
-		nd.config.enemy_data = load(enemy_paths[row]) as EnemyData
-		nd.config.column = 0
-		nd.config.row = row
-		result.append(nd)
-	return result
-
-static func _make_col1() -> Array[NodeData]:
-	var types: Array = [NodeConfig.Type.COMBAT, NodeConfig.Type.REST]
-	types.shuffle()
+		nd.config.enemy_data = load(BOSS_ENEMY_PATH) as EnemyData
+		nd.config.reward_relic = load(BOSS_REWARD_RELIC_PATH) as RelicData
+		nd.config.column = col
+		nd.config.row = 0
+		return [nd]
+	var types: Array = _get_column_types(col)
 	var result: Array[NodeData] = []
 	for row in 2:
 		var nd := NodeData.new()
 		nd.config = NodeConfig.new()
 		nd.config.type = types[row]
-		nd.config.column = 1
-		nd.config.row = row
-		if nd.config.type == NodeConfig.Type.COMBAT:
-			nd.config.enemy_data = _random_combat_enemy()
-		result.append(nd)
-	return result
-
-static func _make_col2() -> Array[NodeData]:
-	var combos: Array = [
-		[NodeConfig.Type.COMBAT, NodeConfig.Type.REST],
-		[NodeConfig.Type.COMBAT, NodeConfig.Type.ELITE],
-		[NodeConfig.Type.REST,   NodeConfig.Type.ELITE],
-	]
-	var combo: Array = combos[randi() % combos.size()].duplicate()
-	combo.shuffle()
-	var result: Array[NodeData] = []
-	for row in 2:
-		var nd := NodeData.new()
-		nd.config = NodeConfig.new()
-		nd.config.type = combo[row]
-		nd.config.column = 2
+		nd.config.column = col
 		nd.config.row = row
 		match nd.config.type:
 			NodeConfig.Type.COMBAT:
@@ -82,27 +52,29 @@ static func _make_col2() -> Array[NodeData]:
 		result.append(nd)
 	return result
 
-static func _make_col3() -> Array[NodeData]:
-	var nd := NodeData.new()
-	nd.config = NodeConfig.new()
-	nd.config.type = NodeConfig.Type.COMBAT
-	nd.config.enemy_data = load(BOSS_ENEMY_PATH) as EnemyData
-	nd.config.reward_relic = load(BOSS_REWARD_RELIC_PATH) as RelicData
-	nd.config.column = 3
-	nd.config.row = 0
-	var result: Array[NodeData] = [nd]
-	return result
+static func _get_column_types(col: int) -> Array:
+	if col <= 3:
+		return [NodeConfig.Type.COMBAT, NodeConfig.Type.COMBAT]
+	elif col <= 5:
+		var types: Array = [NodeConfig.Type.COMBAT, NodeConfig.Type.REST]
+		types.shuffle()
+		return types
+	elif col <= 7:
+		var types: Array = [NodeConfig.Type.ELITE, NodeConfig.Type.COMBAT]
+		types.shuffle()
+		return types
+	else:
+		return [NodeConfig.Type.ELITE, NodeConfig.Type.ELITE]
 
 static func _random_combat_enemy() -> EnemyData:
 	return load(COMBAT_ENEMY_PATHS[randi() % COMBAT_ENEMY_PATHS.size()]) as EnemyData
 
-static func _add_shop_node(all_nodes: Array[NodeData]) -> void:
+static func _add_shop_nodes(all_nodes: Array[NodeData]) -> void:
 	var candidates: Array[NodeData] = []
 	for nd: NodeData in all_nodes:
-		if nd.config.column in [1, 2] and nd.config.type != NodeConfig.Type.ELITE:
+		if nd.config.column >= 4 and nd.config.column <= 8 and nd.config.type == NodeConfig.Type.COMBAT:
 			candidates.append(nd)
-	if candidates.is_empty():
-		return
-	var chosen: NodeData = candidates[randi() % candidates.size()]
-	chosen.config.type = NodeConfig.Type.SHOP
-	chosen.config.enemy_data = null
+	candidates.shuffle()
+	for i in mini(2, candidates.size()):
+		candidates[i].config.type = NodeConfig.Type.SHOP
+		candidates[i].config.enemy_data = null
