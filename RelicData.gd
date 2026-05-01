c99c862e9cf90ac9ec986c00315bd813591da57f
Developer <dev@example.com>
2026-05-01 20:46:48 +0800

    feat: add SWORD_INTENT placeholder to RelicData EffectType

--- RelicData.gd
+++ RelicData.gd
@@ -2,7 +2,7 @@ class_name RelicData
 extends Resource
 
 enum Trigger { COMBAT_START, TURN_START }
-enum EffectType { ENERGY, HEAL_HP, BLOCK, DRAW }
+enum EffectType { ENERGY, HEAL_HP, BLOCK, DRAW, SWORD_INTENT }
 enum Source { SHOP, CHEST }
 
 @export var display_name: String = ""
