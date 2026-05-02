6a0e4dbdb668f96bc1ba58dd85ead88534064eb0
Developer <dev@example.com>
2026-05-02 10:53:31 +0800

    feat: add 测试模式 button to CharSelectScreen

--- char_select/CharSelectScreen.gd
+++ char_select/CharSelectScreen.gd
@@ -1,14 +1,19 @@
 extends Control
 
 @onready var _btn_sword: Button = $VBoxContainer/BtnSword
+@onready var _btn_debug: Button = $VBoxContainer/BtnDebug
 @onready var _btn_back: Button = $VBoxContainer/BtnBack
 
 func _ready() -> void:
 	_btn_sword.pressed.connect(_on_sword_pressed)
+	_btn_debug.pressed.connect(_on_debug_pressed)
 	_btn_back.pressed.connect(_on_back_pressed)
 
 func _on_sword_pressed() -> void:
 	GameManager.start_new_run("sword")
+
+func _on_debug_pressed() -> void:
+	GameManager.go_to_debug_deck()
 
 func _on_back_pressed() -> void:
 	GameManager.go_to_menu()
