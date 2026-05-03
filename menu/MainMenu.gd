91456008ea51836f4860fb6cd9955e68e0d6ade0
Developer <dev@example.com>
2026-05-03 20:14:11 +0800

    feat: add 测试战斗 button to MainMenu, bump version to v0.57.0

--- menu/MainMenu.gd
+++ menu/MainMenu.gd
@@ -1,9 +1,14 @@
 extends Control
 
 @onready var _btn_start: Button = $BtnStart
+@onready var _btn_debug: Button = $BtnDebug
 
 func _ready() -> void:
 	_btn_start.pressed.connect(_on_start_pressed)
+	_btn_debug.pressed.connect(_on_debug_pressed)
 
 func _on_start_pressed() -> void:
 	GameManager.go_to_char_select()
+
+func _on_debug_pressed() -> void:
+	GameManager.go_to_test_select()
