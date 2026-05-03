2f1229b8d231b6f7255a992d83d09cab33ade6ef
Developer <dev@example.com>
2026-05-03 20:12:13 +0800

    feat: add TestSelectScreen with all 12 enemy groups

---
+++ menu/TestSelectScreen.gd
@@ -0,0 +1,28 @@
+extends Control
+
+@onready var _vbox: VBoxContainer = $ScrollContainer/VBoxContainer
+@onready var _btn_back: Button = $BtnBack
+
+func _ready() -> void:
+	_btn_back.pressed.connect(_on_back_pressed)
+	var all_paths: Array[String] = []
+	all_paths.append_array(MapGenerator.EARLY_GROUP_PATHS)
+	all_paths.append_array(MapGenerator.MID_GROUP_PATHS)
+	all_paths.append(MapGenerator.ELITE_GROUP_PATH)
+	all_paths.append(MapGenerator.BOSS_GROUP_PATH)
+	for path in all_paths:
+		var group: EnemyGroupData = load(path) as EnemyGroupData
+		var parts: PackedStringArray = PackedStringArray()
+		for e in group.enemies:
+			parts.append(e.display_name)
+		var btn := Button.new()
+		btn.text = " + ".join(parts)
+		btn.custom_minimum_size = Vector2(0, 40)
+		btn.pressed.connect(_on_group_pressed.bind(group))
+		_vbox.add_child(btn)
+
+func _on_group_pressed(group: EnemyGroupData) -> void:
+	GameManager.start_test_combat(group)
+
+func _on_back_pressed() -> void:
+	GameManager.go_to_menu()
