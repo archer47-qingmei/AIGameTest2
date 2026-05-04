191bb0c0dd02a3f0fb6a0e5ed71c20ba5e0f4707
Developer <dev@example.com>
2026-05-04 15:18:07 +0800

    refactor: rename offset to map_position in NodeConfig

--- NodeConfig.gd
+++ NodeConfig.gd
@@ -8,4 +8,4 @@ enum Type { COMBAT, REST, ELITE, SHOP, CHEST }
 @export var column: int = 0
 @export var row: int = 0
 @export var reward_relic: RelicData
-@export var offset: Vector2 = Vector2.ZERO
+@export var map_position: Vector2 = Vector2.ZERO
